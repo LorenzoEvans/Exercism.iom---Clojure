c1ae631d31e6c9a3b07a4a570a446cd342216f29
Lambda_School_Loaner_94 <lambda_school_loaner_94@Lambda-School-Loaner-94s-MacBook-Pro.local>
2019-03-26 12:53:01 -0400

    Adding collatz-conjecture code.

--- clojure/collatz-conjecture/src/collatz_conjecture.clj
+++ clojure/collatz-conjecture/src/collatz_conjecture.clj
@@ -1,5 +1,12 @@
 (ns collatz-conjecture)
 
-(defn collatz [num] ;; <- arglist goes here
+(defn collatz [num]
+  (while (not= num 1)
+    (cond
+      (= num 2) 1
+      (odd? num) (+ (* 3 num) 1)
+      (even? num) (/ num 2)))
+  (println num)
+  (recur num))
+                     ;; <- arglist goes here
   ;; your code goes here
-)
