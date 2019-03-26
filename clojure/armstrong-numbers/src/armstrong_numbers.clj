9bef0ad507f39c5cf68000ed92b4f8d140236655
Lambda_School_Loaner_94 <lambda_school_loaner_94@Lambda-School-Loaner-94s-MacBook-Pro.local>
2019-03-26 11:38:47 -0400

    Adding Exercism.io challenge.

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -1,11 +1,27 @@
-(ns armstrong-numbers)
+(ns armstrong-numbers
+  (:require [clojure.edn :as edn]))
+
+(def edn edn/read-string)
+
+; (defn armstrong? [num]
+;   (def str_num (str num))
+;   ; Turns number into string
+;   (def num_len (count str_num))
+;   (while (not= num_len 0)
+;     (println 2)))
+
 
 (defn armstrong? [num]
   (def str_num (str num))
   ; Turns number into string
   (def num_len (count str_num))
+  (list num) num)
+
+
   ; Grabs length of number in string form, 1-indexed.
   ; ? Mutate num using num_len from here
-  (recur (println (dec num_len))))
 
-    ;; your code goes here
+    ; take the first number, multiply it by num_len
+    ;  dec num_len (to satisfy conditional at some point
+    ; ?: will decrementing num_len throw off multiplying by num_len
+    ; A: Immutability implies no, but watch out for it anyway.
