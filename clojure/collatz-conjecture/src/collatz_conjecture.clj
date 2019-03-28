83117769d90ef0f7fb71cd872fc944e3338b6a6d
Lambda_School_Loaner_94 <lambda_school_loaner_94@Lambda-School-Loaner-94s-MacBook-Pro.local>
2019-03-28 18:10:55 -0400

    Getting closer to a solution, tests still not passing however.

--- clojure/collatz-conjecture/src/collatz_conjecture.clj
+++ clojure/collatz-conjecture/src/collatz_conjecture.clj
@@ -16,18 +16,28 @@
 ;         (odd? num) (+ (* 3 num) 1)
 ;         (even? num) (/ num 2))))
 ;   (recur num))
+;
+; (defn collatz
+;   [num]
+;   (loop [x num]
+;     (if
+;       (= x 1) 1
+;       (cond
+;         (= x 2) 1
+;           (:else
+;             (odd? x) (+ (* 3) 1)
+;             (even? x) (/ num 2)))))
+;   (recur num))
 
-(defn collatz
-  [num]
+(defn collatz [num]
   (loop [x num]
-    (if
-      (= x 1) 1
-      (cond
-        (= x 2) 1
-          (:else
-            (odd? x) (+ (* 3) 1)
-            (even? x) (/ num 2)))))
-  (recur num))
+    (cond
+      (= 1 x) 1
+      (= 2 x) 1
+      :else (recur
+              (if (even? x) (/ x 2)
+                (+ (* 3 x) 1))))))
+
 
 
 
