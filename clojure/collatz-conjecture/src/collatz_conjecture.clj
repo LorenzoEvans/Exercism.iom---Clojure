d2d38b0fb15ee31fb393f18d5a7cb5623c343004
Lambda_School_Loaner_94 <lambda_school_loaner_94@Lambda-School-Loaner-94s-MacBook-Pro.local>
2019-03-28 17:49:56 -0400

    Attempting collatz conjecture with different loop/recur structure.

--- clojure/collatz-conjecture/src/collatz_conjecture.clj
+++ clojure/collatz-conjecture/src/collatz_conjecture.clj
@@ -8,14 +8,28 @@
 ;       (even? num) (/ num 2))))
 ;   (recur num))
 
-(defn collatz [num]
-  (loop [num]
-    (while (not= num 1)
+; (defn collatz [num]
+;   (loop [num]
+;     (while (not= num 1)
+;       (cond
+;         (= num 2) 1
+;         (odd? num) (+ (* 3 num) 1)
+;         (even? num) (/ num 2))))
+;   (recur num))
+
+(defn collatz
+  [num]
+  (loop [x num]
+    (if
+      (= x 1) 1
       (cond
-        (= num 2) 1
-        (odd? num) (+ (* 3 num) 1)
-        (even? num) (/ num 2))))
+        (= x 2) 1
+          (:else
+            (odd? x) (+ (* 3) 1)
+            (even? x) (/ num 2)))))
   (recur num))
+
+
 
                      ;; <- arglist goes here
   ;; your code goes here
