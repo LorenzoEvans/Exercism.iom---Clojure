20b0758b32dd2477da15ba53016196d140d6487b
Lambda_School_Loaner_94 <lambda_school_loaner_94@Lambda-School-Loaner-94s-MacBook-Pro.local>
2019-03-29 22:40:43 -0400

    The structure is correct but I'm not using the right combination of forms for this kind of iterative process.

--- clojure/collatz-conjecture/src/collatz_conjecture.clj
+++ clojure/collatz-conjecture/src/collatz_conjecture.clj
@@ -1,54 +1,13 @@
 (ns collatz-conjecture)
 
-; (defn collatz [num]
-;   ((while (not= num 1)
-;     (cond
-;       (= num 2) 1
-;       (odd? num) (+ (* 3 num) 1)
-;       (even? num) (/ num 2))))
-;   (recur num))
-
-; (defn collatz [num]
-;   (loop [num]
-;     (while (not= num 1)
-;       (cond
-;         (= num 2) 1
-;         (odd? num) (+ (* 3 num) 1)
-;         (even? num) (/ num 2))))
-;   (recur num))
-
-;
-; (defn collatz
-;   [num]
-;   (loop [x num]
-;     (if
-;       (= x 1) 1
-;       (cond
-;         (= x 2) 1
-;           (:else
-;             (odd? x) (+ (* 3) 1)
-;             (even? x) (/ num 2)))))
-;   (recur num))
-
-; (defn collatz [num]
-;   (loop [x num]
-;     (let [_ (println x)]
-;       (cond
-;         (= 1 num) 1
-;         (= 2 num) 1
-;         :else (recur
-;                (if (even? x)
-;                  (/ x 2)
-;                  (+ (* 3 x) 1)))))))
-
 (defn collatz [num]
   (loop [x num]
-    (let [_ (println x)]
+    (let [_ (println x) val 0]
       (cond
         (= num 1) 1
-        (<= x 0) (throw (ArithmeticException.))
-        (= x 2) 1
-        :else (recur
-               (if (even? x)
-                 (/ x 2)
-                 (+ (* 3 x) 1)))))))
+        (<= x 1) val
+        :else (do (inc val)
+                  (recur
+                   (if
+                    (even? x) (/ x 2)
+                    (+ (* 3 x) 1))))))))
