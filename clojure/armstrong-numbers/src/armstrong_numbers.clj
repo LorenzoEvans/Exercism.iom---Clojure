414222454ceb65e724e46901010456cd5ba06178
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-06-14 23:01:05 -0400

    Commented out most code, researching technique for better solution.

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -3,36 +3,39 @@
 
 ; Use reduce for exponentiation, as Math/pow form Java has limited precision,
 ; which is why we were having the issue with the 17 digit number.
+; This expt function will be applied later on in the solution function,
+ ; than we originally assumed.
 
 
 (defn expt
-  ; Pulls in exponentiation fn from Java's Math
   [base exponent]
   (reduce * (repeat base exponent)))
 
-(defn destring [num]
-  (def int-seq
-    (map #(Integer/parseInt (str %))
-         (seq (str num))))
-  int-seq)
+; Try using quot or rem to determine digits instead of casting to string and back.
 
-(defn arm-val [num]
-  (let [seq (destring num)
-        len (count (destring num))]
-    (let [new-seq (map #(expt % len) seq)]
-      new-seq)))
 
-(defn int-check [num]
-  (cond
-    (< 16 (count (arm-val num))) (- (reduce + (arm-val num)) 1)
-    :else (reduce + (arm-val num))))
+(defn arm-val [num])
 
 (defn armstrong? [num]
-  (cond
-    (= (long num) (int-check num)) true
-    :else false))
+  (if
+   (= num (expt num)) true
+   false))
+    ; (defn destring [num]
+   ;   (def int-seq
+   ;     (map #(Integer/parseInt (str %))
+   ;          (seq (str num))))
+   ;   int-seq)
+   ; (defn arm-val [num]
+   ;   (let [seq (destring num)
+   ;         len (count (destring num))]
+   ;     (let [new-seq (map #(expt % len) seq)]
+   ;       new-seq)))
+   ; (defn int-check [num]
+   ;   (cond
+   ;     (< 16 (count (arm-val num))) (- (reduce + (arm-val num)) 1)
+   ;     :else (reduce + (arm-val num))))
 
-; (defn armstrong? [num]
-;   (if
-;    (= (double num) (reduce +' (arm-val num))) true
-;    false))
+   ; (defn armstrong? [num]
+   ;   (cond
+   ;     (= (long num) (int-check num)) true
+   ;     :else false))
