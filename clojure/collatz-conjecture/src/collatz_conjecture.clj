ce804054fd24852a159b95e22022378a5cda2a6f
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-06-14 23:32:19 -0400

    Adding new coding challenge and additional files, working on nucleotide count.

--- clojure/collatz-conjecture/src/collatz_conjecture.clj
+++ clojure/collatz-conjecture/src/collatz_conjecture.clj
@@ -12,27 +12,4 @@
         :else (do
                 (recur
                  (if (even? x) (/ x 2)
-                     (inc (* 3 x))) (inc val))))))); Previous attempts:
- ; (defn collatz [num]
- ;   (loop [x num val 0]
- ;     (let [_ (println x)]
- ;       (cond
- ;         (= num 1) 1
- ;         (<= x 1) val
- ;         :else (recur
- ;                (do (inc val))
- ;                (if (even? x)
- ;                  (/ x 2)
- ;                  (+ (* 3 x) 1)))))))
- ;
- ; (defn collatz [num]
- ;   (loop [x num val 0]
- ;     (let [_ (println val)]
- ;       (cond
- ;         (= num 1) 1
- ;         (<= x 1) val
- ;         :else (do
- ;                 (recur
- ;                  (if (even? x)
- ;                    (/ x 2)
- ;                    (+ (* 3 x) 1)) (inc val)))))))
+                     (inc (* 3 x))) (inc val)))))))
--- clojure/nucleotide-count/src/nucleotide_count.clj
+++ clojure/nucleotide-count/src/nucleotide_count.clj
@@ -1,9 +1,11 @@
 (ns nucleotide-count)
 
-(defn count [nucleotide strand] ;; <- Arglist goes here
+(defn count [nucleotide strand]
+  (let [i n-t-count (len strand)]
+    (while (> n-t-count i))))
+     ;; <- Arglist goes here
   ;; your code goes here
-)
 
-(defn nucleotide-counts [strand] ;; <- Arglist goes here
+
+(defn nucleotide-counts [strand]) ;; <- Arglist goes here
   ;; your code goes here
-)
